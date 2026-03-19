119f32c079226314d1586c2d55879444a8654e74
outoine15 <outoine15@gmail.com>
2026-03-19 22:21:00 +0100

    feat: demo logout (temporaire)

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,10 @@
+import { check_conn_general } from "../scripts/connUtils.js";
+import { LogoutButton } from "../component/logout.js";
+
+
 //verifie si l'utilisateur est connecte, (doit être executer avec tout autre code quand l'utilisateur arrive sur une page)
 check_conn_general();
 
 
-function check_conn_general(){
-    if(!sessionStorage.getItem("user_token") && !sessionStorage.getItem("admin_token" && !sessionStorage.getItem("secretary_token") && !sessionStorage.getItem("doctor_token"))){
-        console.log("non connecté, redirection (main)");
-        window.location.replace("conn.html");
-    } else {
-        console.log("connecté (main)");
-    }
-}
+let loggout_bt = new LogoutButton();
+document.body.appendChild(loggout_bt);
