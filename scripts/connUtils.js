76de5030d7884e472feed01fdd3e2d7bfb8fed0b
FloH2RUnJour <lorian.parent@gmail.com>
2026-03-30 13:34:09 +0200

    add feature : add home secretary page and add css for index page

--- scripts/connUtils.js
+++ scripts/connUtils.js
@@ -3,11 +3,23 @@ import { getCookie } from "/scripts/cookiesUtils.js";
 export function check_conn_connexion(){
     //redirection plus precise possible (en fonction du role)
     const token = getCookie("token");
+    const role = getCookie("role");
     if (!token || token === "undefined" || token === "null"){
         // non connecté
     } else {
+        switch (role) {
+            case "USER":
+                window.location.replace("/home");
+                break;
+            case "SECRETARY":
+                window.location.replace("/home");
+                
+            default:
+                window.location.replace("/home");
+                break;
+        }
         // connecté
-        window.location.replace("/home");
+        
     }
 }
 
