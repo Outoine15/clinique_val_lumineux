bcb1a3ed760986e655e94f8cdd8f1288df6ffc6d
outoine15 <outoine15@gmail.com>
2026-05-03 12:06:11 +0200

    fix: register_page's api reqest, waiting on fix api side

--- scripts/register.js
+++ scripts/register.js
@@ -1,3 +1,6 @@
+import { isEmptyObject } from "../scripts/codeUtils.js";
+import { check_conn_connexion } from "./connUtils.js";
+
 var firstnameInput = document.getElementById("firstnameInput");
 var nameInput = document.getElementById("nameInput");
 var birthdateInput = document.getElementById("birthdateInput");
@@ -11,7 +14,7 @@ registerButton.addEventListener('click', () => {
     var birthdate = birthdateInput.value;
     var mail = mailInput.value;
     var password = passwordInput.value;
-
+    console.log("click");
     if(firstname && name && birthdate && mail && password) { // s'ils sont tous définie
         axios.put("../api/users",
             new URLSearchParams({
@@ -22,9 +25,39 @@ registerButton.addEventListener('click', () => {
                 birthdate: birthdate
             }).toString()
         ).then(response => {
-            var res = response.data;
-            window.location.href("../login");
+            console.log(response.data);
+            if(!isEmptyObject(response.data)){
+                try{
+                    axios.get("../api/users", {
+                        headers: {
+                           Authorization: "Bearer "+response.data.token
+                        }
+                    }).then(response => {
+                        console.log(response);
+                        var res = response.data;
+                        //gestion cas de reponse
+                        if(!isEmptyObject(response.data)){
+                            if(res.success==true){
+                                var token = res.token;
+                                var role = res.role;
+                            
+                                setCookie("token",token);
+                                setCookie("role",role);
+                            }
+                        }
+                        check_conn_connexion();
+                    })
+                      .catch(err => {
+                        console.log("request failed");
+                        console.log(err);
+                    });
+                } catch {
+                    // erreur connexion automatique, redirection sur connexion manuel (si le server n'a pas crash)
+                    window.location.replace("../login");
+                }
+            }
         }).catch(err => {
+            console.log(err);
             // 404 ou 500 -> "une erreur est survenue"
         });
     }
