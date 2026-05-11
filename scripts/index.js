330fdf329b9104ccd07210950a8203fbab4882b4
benjimania74 <Benjamin.Bogdan@etu.univ-smb.fr>
2026-05-11 13:36:31 +0200

    Données page accueil

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ import "../component/rdv_popup/rdv_popup.js";
 import "../component/header/header.js";
 import "../component/planning/planning.js"
 import "../component/footer/footer.js";
+import { isEmptyObject } from "./codeUtils.js";
 
 
 const rdvButtons = document.querySelectorAll(".btn-rdv");
@@ -11,3 +12,28 @@ rdvButtons.forEach(rdvButton => {
         window.location.replace("../calendar");
     })
 });
+
+
+axios.get("../api/doctors").then(response => {
+    var res = response.data;
+    var doctorNb = 0;
+    var domainNb = 0;
+
+    if(!isEmptyObject(res)) {
+        var seenDomains = [];
+        for(var i = 0 ; i < res.length ; i++) {
+            doctorNb++;
+            if(!seenDomains.includes(res[i]["sector"]["id"])) {
+                seenDomains.push(res[i]["sector"]["id"]);
+                domainNb++;
+            }
+        }
+        var doctorInfo = document.getElementById("nb-doc");
+        doctorInfo.innerText = doctorInfo.innerText.replace("X", doctorNb);
+        var domainInfo = document.getElementById("nb-dom");
+        domainInfo.innerText = domainInfo.innerText.replace("X", domainNb);
+    }
+});
+
+
+console.log(document.getElementById("nb-doc").innerText);
